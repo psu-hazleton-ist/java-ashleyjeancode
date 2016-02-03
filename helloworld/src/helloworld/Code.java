56e8edc17bfcaa8627012bfc69f4774884589149
ashleyjeancode <jean_ashley@hotmail.com>
2016-02-03 09:56:57 -0500

    Create Code.java

---
+++ helloworld/src/helloworld/Code.java
@@ -0,0 +1,13 @@
+package helloworld;
+
+public class Code {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World");
+		
+		Byeworld goodbye = new Byeworld("This is the new me!");
+		System.out.println(goodbye.getgoodbye());
+	}
+
+}
